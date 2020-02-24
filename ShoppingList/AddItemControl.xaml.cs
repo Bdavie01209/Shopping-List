29600f07c05e2dae471473ca3323a03854a609da
Blake Davies <bbd@users.campus>
2020-02-24 17:12:53 -0600

    In class work

---
+++ ShoppingList/AddItemControl.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ShoppingList
+{
+    /// <summary>
+    /// Interaction logic for AddItemControl.xaml
+    /// </summary>
+    public partial class AddItemControl : UserControl
+    {
+        public AddItemControl()
+        {
+            InitializeComponent();
+        }
+
+        private void AddItemButton_Click(object sender, RoutedEventArgs e)
+        {
+            var item = ItemToAdd.Text;
+            if (DataContext is ShoppingListData data)
+            {
+                data.AddItem(item);
+                ItemToAdd.Text = "";
+            }
+            
+
+
+        }
+    }
+}
---
+++ ShoppingList/ShoppingListData.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel;
+
+namespace ShoppingList
+{
+    public class ShoppingListData : INotifyPropertyChanged
+    {
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+
+
+        
+
+
+
+
+
+
+
+
+        private List<string> items = new List<string>();
+
+        public IEnumerable<string> Items => items.ToArray();
+
+        public void AddItem(string item)
+        {
+            items.Add(item);
+            PropertyChanged?.Invoke(this,new PropertyChangedEventArgs("Items"));
+        }
+
+        public void RemoveItem(string item)
+        {
+            items.Remove(item);
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Items"));
+        }
+
+    }
+}
